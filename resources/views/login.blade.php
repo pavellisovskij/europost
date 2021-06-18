1db473beca33f68e69c695144b93022f2664478f
pavellisovskij <pavel.lisovskij93@gmail.com>
2021-06-18 18:18:09 +0300

    added small changes and readme.md

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,20 +8,24 @@
                 <div class="card-header">Вход</div>
 
                 <div class="card-body">
-                    @if(session('error'))
-                        <p class="alert alert-danger">{{ session('error') }}</p>
-                    @endif
+                    @if(!session('user'))
+                        @if(session('error'))
+                            <p class="alert alert-danger">{{ session('error') }}</p>
+                        @endif
 
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                        <div class="input-group mb-3">
-                            <input type="password" name="password" class="form-control" placeholder="Пароль" required>
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="submit">Войти</button>
+                            <div class="input-group mb-3">
+                                <input type="password" name="password" class="form-control" placeholder="Пароль" required>
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">Войти</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    @else
+                        <p class="alert alert-secondary">Вы уже вошли в систему. <a href="{{ route('home') }}">К списку адресов.</a></p>
+                    @endif
                 </div>
             </div>
         </div>
